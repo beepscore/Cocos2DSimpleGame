9a3a16c3366d73ae7a9ede01c88401647c5c693e
beepscore <support@beepscore.com>
2010-10-09 00:27:47 -0700

    Add targets, move them across the screen, and remove them.

--- Classes/HelloWorldLayer.m
+++ Classes/HelloWorldLayer.m
@@ -9,6 +9,16 @@
 // Import the interfaces
 #import "HelloWorldLayer.h"
 
+// declare anonymous category for "private" methods, avoid showing in .h file
+// Note in Objective C no method is private, it can be called from elsewhere.
+// Ref http://stackoverflow.com/questions/1052233/iphone-obj-c-anonymous-category-or-private-category
+
+@interface HelloWorldLayer ()
+-(void)gameLogic:(ccTime)dt;
+-(void)spriteMoveFinished:(id)sender;
+-(void)addTarget;
+@end
+
 // HelloWorld implementation
 @implementation HelloWorldLayer
 
@@ -37,10 +47,59 @@
         CCSprite *player = [CCSprite spriteWithFile:@"Player.png" 
                                                rect:CGRectMake(0, 0, 27, 40)];
         player.position = ccp(player.contentSize.width/2, winSize.height/2);
-        [self addChild:player];		
+        [self addChild:player];
+        
+		[self schedule:@selector(gameLogic:) interval:1.0];
     }
 	return self;
 }
+
+
+-(void)gameLogic:(ccTime)dt {
+    [self addTarget];
+}
+
+
+-(void)spriteMoveFinished:(id)sender {
+    CCSprite *sprite = (CCSprite *)sender;
+    [self removeChild:sprite cleanup:YES];
+}
+
+
+-(void)addTarget {
+    
+    CCSprite *target = [CCSprite spriteWithFile:@"Target.png" 
+                                           rect:CGRectMake(0, 0, 27, 40)]; 
+    
+    // Determine where to spawn the target along the Y axis
+    CGSize winSize = [[CCDirector sharedDirector] winSize];
+    int minY = target.contentSize.height/2;
+    int maxY = winSize.height - target.contentSize.height/2;
+    int rangeY = maxY - minY;
+    int actualY = (arc4random() % rangeY) + minY;
+    
+    // Create the target slightly off-screen along the right edge,
+    // and along a random position along the Y axis as calculated above
+    target.position = ccp(winSize.width + (target.contentSize.width/2), actualY);
+    [self addChild:target];
+    
+    // Determine speed of the target
+    int minDuration = 2.0;
+    int maxDuration = 4.0;
+    int rangeDuration = maxDuration - minDuration;
+    int actualDuration = (arc4random() % rangeDuration) + minDuration;
+    
+    // Create the actions
+    // move off the left side of the screen
+    id actionMove = [CCMoveTo actionWithDuration:actualDuration 
+                                        position:ccp(-target.contentSize.width/2, actualY)];
+    
+    id actionMoveDone = [CCCallFuncN actionWithTarget:self 
+                                             selector:@selector(spriteMoveFinished:)];
+    
+    [target runAction:[CCSequence actions:actionMove, actionMoveDone, nil]];    
+}
+
 
 // on "dealloc" you need to release all your retained objects
 - (void) dealloc
